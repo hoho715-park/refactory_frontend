2920b0699c3a007f38c25abc5549661cd5216ae5
hoho715 <andy2001@g.hongik.ac.kr>
2026-03-09 16:15:40 +0900

    main-hero 이미지 메인페이지 백그라운드 이미지로 생성

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import './MainPage.css';
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      <section className="hero-section">
+        {/* Hero content will be added in subsequent development */}
+      </section>
+    </div>
+  );
+}
+
+export default MainPage;
